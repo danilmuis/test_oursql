fff2cd7f7d9ee2a7b52a969e6bc547425ce8d678
antykasyahrial <antykass2@gmail.com>
2021-04-13 10:38:58 +0700

    migrating table method and subject

---
+++ laravel/database/migrations/2021_04_13_033242_create_subjects_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateSubjectsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('subjects', function (Blueprint $table) {
+            $table->id();
+            $table->string('subjectsName');
+            $table->date('startDate');
+            $table->date('endDate');
+            $table->bigInteger('idMethod')->unsigned();
+            $table->foreign('idMethod')->references('id')->on('methods')->onUpdate('cascade')->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('subjects');
+    }
+}
